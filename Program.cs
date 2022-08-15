88289aef62799c7d2180fa003cc561b0691a4618
mani1 <mani1@NANI143>
2022-08-15 18:45:03 +0530

    [Rajeswari]Added Anagram Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Anagram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Anagram anagram = new Anagram();
+            anagram.checkAnagram();
+        }
+    }
+}
