2d96f3cc154c1eeee0ddb2ad9bfffd7e2fe40620
mani1 <mani1@NANI143>
2022-08-15 11:13:43 +0530

    [Rajeswari]Added PermutationOfString

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DataStructureandAlgorithm
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
+                Console.WriteLine("Welcome To Datastructures and Algorithms");
+                Console.WriteLine("1.Permutation Using Recursion");
+                Console.WriteLine("Enter Your Option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        PermutationsOfString permutationsofstring = new PermutationsOfString();
+                        permutationsofstring.permutations();
+                        break;
+
+                    default:
+                        Console.WriteLine("-------Enter the valid option---");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
