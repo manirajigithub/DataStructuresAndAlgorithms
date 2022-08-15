07f1d732f5dab5e5fcb359655736614e987d2370
mani1 <mani1@NANI143>
2022-08-15 18:36:50 +0530

    [Rajeswari]ABubbleSort Part Has Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BubbleSortDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BubbleSort bubbleSort= new BubbleSort();
+            bubbleSort.Sorting();
+        }
+    }
+}
